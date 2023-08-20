89754cc676bb96a9d9791ed2a4221c4f65cc7908
Lsccccc <142605575+Lsccccc@users.noreply.github.com>
2023-08-20 18:00:01 +0800

    Update translator.py
    Fixed incorrectly line dividing bug.

--- translator.py
+++ translator.py
@@ -5,6 +5,13 @@ from time import sleep
 from random import random
 
 
+def remove_all(element, l: list):
+    l2 = []
+    for i in l:
+        if i != element:
+            l2.append(i)
+    return l2
+
 def get_meanings(kw):
     args = parse.urlencode({'word': kw, 'lang': 'en'})
     url = f'https://dict.youdao.com/result?{args}'
@@ -13,12 +20,16 @@ def get_meanings(kw):
     html = res.read().decode('UTF-8')
     doc = etree.HTML(html)
     Xpath = doc.xpath('/html/body/div/div/div/div/div[1]/div/div/section/div[2]/div[2]/div/div/ul//li//span/text()')
-    meanings = f'\n--------------------\n{kw}'
+    class_list = doc.xpath('/html/body/div/div/div/div/div[1]/div/div/section/div[2]/div[2]/div/div/ul//li//span/@class')
+    class_list = remove_all("wfs-splice", class_list)
+    meanings = f'\n--------------------\n{kw}\n'
+    flag = 0
     for i in range(len(Xpath)):
-        if i%2 == 0:
-            meanings+='\n'
         meanings += Xpath[i]
+        if class_list[i] in ["trans", "transformation"]:
+            meanings += '\n'
     return meanings
+
 
 with open('vocab.txt', 'r', encoding='UTF-8') as f:
     lines = f.readlines()
